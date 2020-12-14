9a833994dc0681d9cdc28490c87ea72ba545dc4a
Mary Allysa <40545337+alymisola@users.noreply.github.com>
2020-12-14 13:11:19 -0500

    Added new files

---
+++ myscript.js
@@ -0,0 +1,26 @@
+// function bigImg(x) {
+//   x.style.height = "500%";
+//   x.style.width = "500%";
+//   x.src = 'images/purple.png';
+// }
+
+// function normalImg(x) {
+//   x.style.height = "100%";
+//   x.style.width = "100%";
+// }
+
+function mouseOverImage() {
+  document.getElementById("img").src = "images/LTOrange.png";
+}
+
+function mouseOutImage() {
+  document.getElementById("img").src = "images/LTBackground.png";
+}
+
+// function mouseOver() {
+//   document.getElementById("demo").style.color = "red";
+// }
+
+// function mouseOut() {
+//   document.getElementById("demo").style.color = "black";
+// }
